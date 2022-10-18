6f8b33535a328d1a9c648d6447d02c72d08cc0bb
Mohanad Marzouk <mohaned.maher@gmail.com>
2022-10-18 11:40:57 +0200

    Add files via upload

---
+++ MCAL/DIO_config.h
@@ -0,0 +1,47 @@
+/*
+* DIO_config.h
+*
+* Created: 4/8/2022 2:06:07 PM
+*  Author: the samoray
+*/
+
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+
+
+typedef enum {DIO_PORTA,
+	DIO_PORTB,
+	DIO_PORTC,
+DIO_PORTD} PORT_TYPE;
+
+
+#define PIN0   0
+#define	PIN1   1
+#define	PIN2   2
+#define	PIN3   3
+#define	PIN4   4
+#define	PIN5   5
+#define	PIN6   6
+#define	PIN7   7
+
+
+//typedef enum {PIN0,PIN1,PIN2,PIN3,PIN4,PIN5,PIN6,PIN7} PIN_TYPE;
+
+typedef unsigned char PIN_TYPE;
+
+typedef enum {LOW_PIN, HIGH_PIN} STATE_TYPE;
+
+typedef enum {INPUT_PIN,OUTPUT_PIN} DIR_TYPE;
+
+typedef enum {Data_H_To_Port_H,
+	Data_L_To_Port_H,
+	Data_H_To_Port_L,
+Data_L_To_Port_L} NIPPLE_TYPE;
+
+
+
+
+
+#endif /* DIO_CONFIG_H_ */
---
+++ MCAL/DIO_interface.h
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+/*
+* DIO.h
+*
+* Created: 5/26/2021 12:44:58 PM
+*  Author: EN.ESLAM EID
+*/
+
+
+#ifndef DIO_INTERFACE_H_
+#define DIO_INTERFACE_H_
+
+#include "DIO_private.h"
+#include "DIO_config.h"
+#include "../BITMATH.h"
+
+#define OFF 0
+#define ON  1
+
+
+void DIO_SetPinDir(PORT_TYPE , PIN_TYPE , DIR_TYPE );
+
+void DIO_SetPinVal(PORT_TYPE , PIN_TYPE , STATE_TYPE);
+
+void DIO_TogglePin(PORT_TYPE , PIN_TYPE );
+
+STATE_TYPE DIO_GetPinVal(PORT_TYPE,PIN_TYPE);
+
+void DIO_SetPullup(PORT_TYPE , PIN_TYPE );
+
+
+void DIO_SetPortNibble(PORT_TYPE , Uint8 Data,NIPPLE_TYPE );
+
+
+
+#endif
