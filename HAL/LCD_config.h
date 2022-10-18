be378a3dcde120deedf47b2e154a22c04903708b
Mohanad Marzouk <mohaned.maher@gmail.com>
2022-10-18 11:41:54 +0200

    Add files via upload

---
+++ HAL/LCD_config.h
@@ -0,0 +1,31 @@
+/*
+ * LCD_CFG.h
+ *
+ * Created: 6/7/2021 11:59:10 AM
+ *  Author: EN.ESLAM EID
+ */ 
+
+
+#ifndef LCD_CFG_H_
+#define LCD_CFG_H_
+
+#include "../MCAL/DIO_interface.h"
+
+
+
+
+#define LCD_CMD_PORT                 DIO_PORTB
+#define RS_PIN                       PIN1
+#define RW_PIN                       PIN2
+#define E_PIN                        PIN3
+
+
+#define LCD_DATA_PORT                DIO_PORTA
+#define LCD_PIN4                     PIN4
+#define LCD_PIN5                     PIN5
+#define LCD_PIN6                     PIN6
+#define LCD_PIN7                     PIN7
+
+#define LCD_LINE_MODE			LCD_4Bit_Mode_2_Line_5X8
+
+#endif /* LCD_CFG_H_ */
